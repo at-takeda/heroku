a23c1d52146304bd452051f3a10877c2b809a967
武田敦 <takeda@takeda-MacBook-Air.local>
2016-05-16 14:30:11 +0900

    first commit

---
+++ slack-weather-notifier.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'open-uri'
+require 'slack/incoming/webhooks'
+
+uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+
+res     = JSON.load(open(uri).read)
+title   = res['title']
+link    = res['link']
+weather = res['forecasts'].first
+
+
+slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
+slack.post "<#{link}|#{weather['date']}の#{title}>は「#{weather['telop']}」です。"
+
